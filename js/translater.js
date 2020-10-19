53a98c5ba154ef64e8b6d2e200b05b0fa3831436
suganorm <73138626+suganorm@users.noreply.github.com>
2020-10-19 23:35:17 +0300

    1

---
+++ js/translater.js
@@ -0,0 +1,277 @@
+var new_lang = {
+"lt0": "Természetes termék, amely normalizálja a vércukorszintet ",
+
+"lt1": "Ingyenes hívás",
+
+"lt2": "Normalizálja",
+
+"lt3": "a vércukor szintet",
+
+"lt4": "Régi ár <span class=\"price_old\">000 рублей</span>",
+
+"lt5": "Akciós ár <span class=\"price_main\">000 рублей</span>",
+
+"lt6": "Rendelés",
+
+"lt7": "Eredmény már 1 kúra után ",
+
+"lt8": "Teljesen biztonságos, mellékhatások nélkül",
+
+"lt9": "Jelentősen bővíti az étrendet",
+
+"lt10": "Szakembereink segítsége és tanácsa a <br>kúr aegész ideje alatt",
+
+"lt11": "Rendelés ingyenes kiszállítással",
+
+"lt12": "Ingyenes tanácsadás",
+
+"lt13": "Régi ár:",
+
+"lt14": "Új ár:",
+
+"lt15": "Adja meg az adataidat az alábbi űrlapon",
+
+"lt16": "Rendelés kedvezménnyel",
+
+"lt17": "Ingyenes konzultáció",
+
+"lt18": "A vásárlás előtt és semmi kötelezettség nélkül",
+
+"lt19": "Minden szakember naponta 20-30 fogyasztót támogat a táplálkozás és a termék befogadása terén. És hogy időt jusson mindenkinek, növeltük a hitelesített szakemberek számát a vásárlás előtt történő konzultáció céljából.",
+
+"lt20": "Ma maradt",
+
+"lt21": "konzultáció",
+
+"lt23": "Milyen eredményeket értünk el",
+
+"lt24": "A vércukorszint stabilizálása 2 hét alatt",
+
+"lt25": "Az emberek 87% -a látható eredményeket ért el 1 hónap használat után",
+
+"lt26": "Az ügyfelek több mint 90% -a stabilan meghosszabbítja a cselekvést",
+
+"lt27": "A vásárolt gyógyszerek költségvetésének csökkentése 70% -ra",
+
+"lt28": "A SugaNorm-al<br> könnyebbé válik az eredmény elérése - Kezdje most!",
+
+"lt29": "Az egészség felé vezető út az SugaNorm-al",
+
+"lt30": "Felelőtlen út",
+
+"lt31": "Rendeljen most Magyarországon",
+
+"lt33": "Milyen cukorbetegség tünetek léteznek?",
+
+"lt34": "Krónikus fáradtság",
+
+"lt35": "Csökkent látásélesség",
+
+"lt36": "Megmagyarázatlan tömegváltozás",
+
+"lt37": "A sebek nem gyógyulnak sokáig,",
+
+"lt38": "Szexuális problémák",
+
+"lt39": "Zsibbadás, bizsergés a lábakban vagy a kezekben",
+
+"lt40": "Állandó száraz száj",
+
+"lt41": "Gyakori vizelés",
+
+"lt42": "Állandó éhség",
+
+"lt43": "Migrén és fejfájás",
+
+"lt44": "Az ok a magas vércukorszint",
+
+"lt46": "Normalizálja a vércukrot",
+
+"lt47": "Megszünteti a krónikus fáradtságtöl",
+
+"lt48": "Megállítja a látás elvesztését",
+
+"lt49": "Normalizálja a súlyt",
+
+"lt50": "Visszaállítja a szövetek anyagcseréjét",
+
+"lt51": "Visszatér a szexuális vonzerő",
+
+"lt52": "Normalizálja a vérkeringést a végtagokban",
+
+"lt53": "Megszünteti a megmagyarázhatatlan szomjúság érzését",
+
+"lt54": "Stabilizálja a húgyutak működését",
+
+"lt55": "Megszünteti az állandó éhség érzését",
+
+"lt56": "Megakadályozza a fejfájást",
+
+"lt57": "Minél előbb normalizálni a cukorszintet, annál kevesebbet kell inni a gyógyszereket - Kezdje most!",
+
+"lt58": "SugaNorm - a cukorbetegség elleni küzdelem gyógyszere",
+
+"lt59": "KEDVEZMÉNY",
+
+"lt63": "Termékek száma<br> az önhöz legközelebbi raktáron <br>korlátozott!",
+
+"lt66": "Rendeljen most Magyarországon",
+
+"lt68": "Mi fog történni, ha elhalasztani a cukorbetegség kezelését",
+
+"lt69": "Nephropathia",
+
+"lt70": "Angiopathia",
+
+"lt71": "Polineuropátia",
+
+"lt72": "Retinopathia",
+
+"lt73": "Ketoacidózis",
+
+"lt74": "<strong>Nephropathia</strong> a vesék szűrőelemei (glomerulusok és tubulusok) diabéteszes elváltozásai, valamint az azokat tápláló edények. A diabeteses nephropathia - betegek korai elhalálozásának és fogyatékosságának egyik gyakori oka. A cukorbetegség - nem az egyetlen oka a veseproblémáknak. De azok között, aki dialízisben szenved, és egy donorvese kapcsán áll a transzplantációra, a cukorbetegek leginkább. ",
+
+"lt75": '<strong>Angiopathy</strong> a cukorbetegség szövődménye, amely az emberi test összes véredényének vereségében nyilvánul meg. a "cukrok magas számjegye" hatására, az edényeken áthaladva, az artériák, az erek és a kis kapillárisok falai megsemmisülnek. Ennek kapcsán kialakul a környező szövetek hipoxia (oxigén éhezése), és a betegek számos szerveit is érintett. A magas fokú angiopathia fekélyek megjelenéséhez, szöveti nekrózishoz (gangréna) és végtagamputációhoz vezet. Ha egy orvos ajánlásait nem észlelik, az angiopátia kialakulásától számított 5 éven belül a betegség 90% -a fejlődik ki.',
+
+"lt76": "<strong>Polineuropátia</strong>— a perifériás idegek többszörös veresége, részleges bénulásnak, érzékenységi rendellenességeknek és más betegségeknek nyilvánul meg, amelyek főként a páciens testének alsó végtagjaiban fordulnak elő. A betegség későbbi szakaszaiban az alsó végtagok polineuropátiája izomsorvadáshoz, trophikus fekélyek megjelenéséhez és a lábak bénulásához, majd a légzőizmok kialakulásához vezethet. ",
+
+"lt77": "<strong>Retinopathia</strong> a szemgolyó retinájának edeteinek veresége. A cukorbetegség komoly és nagyon gyakori szövődménye, ami vaksághoz vezethet. A diabéteszes páciensek 85% -ánál észleltek szövődményeket. Amikor a 2-es típusú cukorbetegség középkorú és idős embereknél fordul elő, akkor az esetek több mint 50% -ában azonnal észlelik a vér véredényeinek elváltozását, ami a szem vérét táplálja. Ez a leggyakoribb oka a 20 és 74 év közötti felnőttek közötti vakság új esetének. ",
+
+"lt78": "<strong>Ketoacidosis</strong>— akut szövődmény, amely az inzulinhiány miatt alakul ki. A sejtek nem használhatják a glükózt energiaforrásként, és a szervezet a zsírraktárakat táplálja. A zsírok feldarabolása során ketontesteket (ketonokat) termelnek. Ha túl sok keton indul el a vérben, akkor a veséknek nincs ideje eltávolítani őket a testből, a vér savasságát emeli. Az ember kezd gyengeséget érezni, és kómába eshet. Ez a helyzet sürgős orvosi beavatkozást igényel, mivel gyakran halálhoz vezet. ",
+
+"lt79": "Ne húzza - mentse az egészségét",
+
+"lt81": "Segít elkerülni a következményeket",
+
+"lt82": "<strong>Megakadályozza a nephropathiát</strong>—a szervezetben a szénhidrát-anyagcsere helyreállítása miatt nincs szükség arra, hogy a vesék túlzott terhet vegyenek fel a felesleges glükóz eltávolításával a testből. ",
+
+"lt83": "<strong>Megakadályozza a angiopathyt</strong>— mivel a vércukorszint csökkentése lehetővé teszi az edények rugalmasabbá tételét, helyreállítja természetes vezetőképességét. ",
+
+"lt84": "<strong>Megakadályozza a polyneuropathiát</strong>— segít megelõzni az idegrendszer vereségét a glükóz toxikus hatásának csökkentésével a szervezet összes szervére és szövetére. ",
+
+"lt85": "<strong>Megakadályozza a retinopátia kialakulását</strong>— visszaállítja a látóideg táplálását a szemedények megfelelő működésének köszönhetően. ",
+
+"lt86": "<strong>Megakadályozza a ketoacózist</strong>— teljes testet biztosít a test sejtjeinek glükózzal, ami lehetővé teszi a test mérgezésének elkerülését. ",
+
+"lt87": "Minél korábban normalizálják a cukorszintet, annál kisebb a veszélyes következmények - Kezdje most!!",
+
+"lt88": "SugaNorm - a cukorbetegség elleni küzdelem gyógyszere",
+
+"lt93": "Termékek száma<br> az önhöz legközelebbi raktáron <br>korlátozott!",
+
+"lt96": "Rendeljen most Magyarországon",
+
+"lt98": "Hogyan kezdeni használni az SugaNorm-t",
+
+"lt99": "Ön hagy egy kérelmet az oldalon vagy telefonon. ",
+
+"lt100": "A tanácsadó visszahívja",
+
+"lt101": "Elemezzük az Ön egészségi állapotát",
+
+"lt102": "Meghatározzuk a kúrát",
+
+"lt103": "Szállítás a legközelebbi postahivatalba",
+
+"lt104": "Fizetés a szállítás után",
+
+"lt105": "Szakembere kíséri Önt a kúra során",
+
+"lt106": "Az Ön egészség és hosszú élettartama",
+
+"lt107": "Önnel csak a profi szakemberek dolgoznak",
+
+"lt108": "Balog Bálint",
+
+"lt109": "A vállalat vezető szakembere, 1-es típusú cukorbeteg, több mint 5 éve dolgozik a cégnél, több mint 1500 konzultációt folytatott.",
+
+"lt110": "Konzultációt kapni Bálinttól",
+
+"lt111": "Vélemények Bálintról",
+
+"lt112": "Máté Viktor, 53 éves, Szolnok",
+
+"lt113": "Külön köszönetet szeretnék kifejezni Balog Bálintnak. A személy elsőként tudja a cukorbetegséggel kapcsolatos problémákat. Cukorbeteg vagyok, sokáig. De komoly problémák kezdődtek az elmúlt évben. A férfiak megértenek engem: sikertelen volt a férfi egészség szempontjából. A családnak problémái voltak. És mindez állandó fejfájás, álmatlanság, ingerlékenység, és ennek eredményeként - apátia hátterében. Ez év tavaszától kezdve SugaNorm-t szedek. És ha kezdetben szkeptikus voltam, most néhány hónap múlva elmondhatom, hogy javaslom az SugaNorm-t mindazoknak, akik korábban a cukorbetegség elleni küzdelemben ugyanazt a fiasco-t szenvedték, mint én. Javult a családi élet, Balog Bálintnak köszönhetően. A munkahelyen nemrég kaptam emelést.",
+
+"lt114": "Biró Anikó, 41 éves, Pécs",
+
+"lt115": "Sokáig kutattam a módját a vércukor stabilizálására. Cukorbeteg vagyok több mint 30 éve. Nyomás, gyenge látás, a vesék és máj jelentéktelen munkája - mindez a múltban. SugaNorm több mint fél évet szedek Balog Bálint felügyelete mellett. Nemcsak hétköznapon ellenőrzi a gyógyszert, de egyéni étrendet is adott, aminek köszönhetően még több kilót vesztettem. Még egyszer köszönetet mondok Balog Bálintnak a probléma világos megértésében és az erkölcsi támogatásban a rettenetes betegség elleni küzdelemben. ",
+
+"lt116": "Hajdu Nóra, 36 éves, Debrecen",
+
+"lt117": 'A késői súlyos terhesség, a vészhelyzetben lévő császármetszés és a diabéteszes kóma , megelőzte a "Diabetes Diabetes" diagnózisát. Okkor az általános állapotom a következőképpen írható le: súlyosan beteg anya és kisgyermek. Nagyon örülök, hogy mindez a múltban van. Balog Bálint valóban megkérte a problémámat, és az SugaNorm-al együtt jó eredményeket értünk el az elmúlt 4 hónapban. Nos, a baba örömmel látja vidám anyját.',
+
+"lt118": "Hajdu Inga",
+
+"lt119":  "2-es típusú cukorbetegség specialistája négy éve dolgozik a cégnél, több mint 900 ügyfelet segített",
+
+"lt120": "Konzultációt kapni Ingától",
+
+"lt121": "Vélemények Ingáról",
+
+"lt122": "Molnár Erik, 74 éves, Siófok",
+
+"lt123": "Cukorbeteg több mint 20 éve. Mindent megpróbáltam, amit az orvos tanácsolt, valamint a hagyományos orvoslás receptjeit. Az eredmény ugyanaz: a vércukorszint nem szakadt meg. Az egészségügyi állapot romlott. Abbahagyta a kilépést, egyszerűen nem volt erő. SugaNorm segített nekem, hogy magammal találjak erőt. Így találkoztam Hajdu Ingával. Eddig több mint hat hónapig ismerjük egymást, és az öröm nem szűnik meg! A cukor a normán belül van. Az életerő visszaállt, és nem csak az utcára megyek, hanem a kedvenc sakkklubomba is. Kedves Hajdu Inga, őszintén köszönöm érzékenységét, kedvességét, megértését és meleg emberi hozzáállását! ",
+
+"lt124": "Sipos Rita, 48 éves, Budapest",
+
+"lt125": 'Egy szörnyű baleset után számos műtétet végeztek, és ennek eredményeként szörnyű diagnózist - cukorbetegség. 2003 óta az inzulinnal vagyok. Még csaknem 40 éves voltam, de úgy éreztem, hogy egész évszázadot éltem. Amint a vércukor szintje megemelkedett, a látás elkezdett esni, a cukorbetegség diagnózisa mellé, mint nekem akkoriban, a magas vérnyomás "ragasztott". De ahogy mondják: a fuldokló emberek megmentése - maguk a fuldokló munkája - úgy döntöttek, hogy megmentik magukat. Segített az internet. Az egyik oldalon olvastam egy olyan történetet, mint az enyém, ahol egy cukorbeteg beteg SugaNorm-et használt, és jelentős eredményeket ért el a betegsége elleni küzdelemben. A cukorbetegségem elleni "háborúm" még nem fejeződött be, de SugaNorm-nek köszönhetően vissza tudtam térni a munkába, az unokáimkal foglalkozok, és jövőre tervezek egy csehországi kirándulást. Hálás köszönetet mondok Hajdu Ingának és SugaNorm-nek, akik az élet "ízét" visszaküldték!',
+
+"lt126": "Gál Noémi, 62 éves, Szeged",
+
+"lt127": "Hajdu Inga szakember nagy nagybetűvel! Знающий, грамотный и понимающий специалист. Munkája - példa lehet bármely szakembernek. Az én történetem több száz ezer másra hasonló: a cukorbetegségre való genetikai hajlamot súlyosbította a káros termelésre gyakorolt hatás. 2007-ben a kezelőorvos diagnózist készített: cukorbetegség. Néhány gyógyszer helyettesítésének sorrendje következett. Őszintén semmi sem segített nekem. Az állapot romlott: légszomj, fejfájás, szédülés és szájszárazság volt. Ez egy kis lista, amit tapasztaltam. A SugaNorm-ról véletlenül az endokrinológus irodájában tudtam meg. És a nő, aki beszélt róla, vizuálisan kitűnt a cukorbetegek általános tömegéből. Úgy döntöttem, hogy nem adok fel, és felkerestem az SugaNorm oldalt. 6 hónapos kúrát ajánlottak fel. A mai napig a glükóz szintje 5,8, jól érzem magam, minden teszt normális, és ezért ismét köszönetet mondok az SugaNorm-nak és Hajdu Ingának! ",
+
+"lt128": "Nagy Klára",
+
+"lt129": "dolgozik a cégben az alapítás napjától,  legtöbb beteget vezet.",
+
+"lt130": "Barna Anikó",
+
+"lt131": "a leginkább kompetensen segít cukorbeteg diétát készíteni.",
+
+"lt132": "Orosz Katalin",
+
+ "lt133": "tapasztalt szakember, kiterjedt tudással rendelkezik.",
+
+"lt134": "Gáspár Margit",
+
+"lt135": "szükség szakértő a cukorbetegség szövődményeinek területén",
+
+"lt136": "Nagyon könnyebb az SugaNorm-al",
+
+"lt137": "Biztonságos és nincsenek mellékhatásai",
+
+"lt138": "Nem okoz függőséget",
+
+"lt139": "Univerzális mindkét típusú cukorbetegek számára",
+
+"lt140": "Hozzáférhető és gazdaságos",
+
+"lt141": "SugaNorm - a cukorbetegség elleni küzdelem gyógyszere",
+
+"lt146": "Termékek száma<br> az önhöz legközelebbi raktáron <br>korlátozott!",
+
+"lt151": "Visszahívás",
+
+"lt152": "Hagyja a kérelmet, és szakértőnk további részletekért lép fel kapcsolatba",
+
+"lt154": "SugaNorm - a cukorbetegség elleni küzdelem gyógyszere",
+
+"lt159": "Termékek száma<br> az önhöz legközelebbi raktáron <br>korlátozott!",
+
+"lt162": "SugaNorm - a cukorbetegség elleni küzdelem gyógyszere",
+
+"lt167": "Termékek száma<br> az önhöz legközelebbi raktáron <br>korlátozott!",
+
+"lt170": "Köszönjük a megrendelését",
+};
+function Translater () {
+               for (class_name in new_lang) {
+                var elements = document.getElementsByClassName(class_name);
+                if (elements.length) {
+                 for (key in elements) {
+                  elements[key].innerHTML = new_lang[class_name];
+                 }
+              }
+            }
+          };
